69699c6f7034c47deee1bf18aae7e110802769fb
TegHocine <arcou98@hotmail.com>
2022-06-23 22:59:59 +0100

    ui skeleton and folder structure

---
+++ src/components/image/Image.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Image.css'
+
+const Image = () => {
+  return <div>Image</div>
+}
+
+export default Image
---
+++ src/components/title/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Title.css'
+
+const Title = () => {
+  return <div>Title</div>
+}
+
+export default Title
